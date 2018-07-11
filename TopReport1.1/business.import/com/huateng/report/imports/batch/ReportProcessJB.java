d3fb2897ad221a327f07a2aeec513532ca90f3f6
alca <alca@dingxuebo-mpc>
2018-07-11 16:05:57 +0800

    增加金标独立生成报送文件功能

--- TopReport1.1/business.import/com/huateng/report/imports/batch/ReportProcessJB.java
+++ TopReport1.1/business.import/com/huateng/report/imports/batch/ReportProcessJB.java
@@ -1,11 +1,25 @@
 package com.huateng.report.imports.batch;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+import com.huateng.ebank.framework.report.common.ReportConstant;
 import com.huateng.ebank.framework.util.DataFormat;
 import com.huateng.ebank.framework.util.DateUtil;
+import com.huateng.report.imports.common.Constants;
+import com.huateng.report.utils.PackZipUtil;
+import com.huateng.report.utils.ReportUtils;
 
+import east.dao.BaseDao;
 import east.dao.JBDao;
+import east.utils.tools.ToolUtils;
+import east.utils.tools.XmlUtil;
 
 /**
  * 生成金标数据主类
@@ -35,6 +49,12 @@ public class ReportProcessJB {
 			generateCLoanBalance(monthEndDate);
 			generateCLoanAccrual(monthStartDate, monthEndDate);
 		}
+		try{
+			generateJBReportFile();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
 	}
 	
 	/*
@@ -78,5 +98,83 @@ public class ReportProcessJB {
 	private void generateCLoanAccrual(String monthStartDate, String monthEndDate) throws Exception{
 		jbDao.insertCLoanAccrual(monthStartDate, monthEndDate);
 	}
+	
+	private void generateJBReportFile() throws Exception{
 
+		//取任务日期
+		String[] args = ToolUtils.queryDate();
+		System.out.println("Start generate JB report files:"+args[0]);
+		XmlUtil x = new XmlUtil();
+		Map<String, List<String>> tableInfoMap = BaseDao.queryFieldInfo();
+		//读取sql.xml中sql
+		Map<String, String> sqlMap = x.getSqlMap();
+		double start;
+		double end ;
+		//金标-金融机构编码
+		String jbBankCode = BaseDao.getJBBankCode();
+		
+		//生成金标文件路径加年月日路径
+		String jbFilePath = ReportUtils.getSysParamsValue(Constants.PARAM_DIR,
+				Constants.PARAM_DIR_0104, "");
+//		String jbFilePath = "C:\\Project\\EAST"; //本地测试
+		jbFilePath = jbFilePath + File.separator + args[0].substring(0, 6) + File.separator;
+		File jbPath = new File(jbFilePath);
+		if(!jbPath.exists()){
+			jbPath.mkdir();
+		}
+				
+		Map retMap;
+		Boolean retFlag = false;
+		String retType = "";
+		Set<String> jbSet = new HashSet<String>();
+		for (String tableName : tableInfoMap.keySet()) {
+
+			//判断某报表在当前日期是否执行数据分析生成文件
+			retMap = BaseDao.ifCreateFile(tableName, args[0]);
+			retFlag = (Boolean)retMap.get("retFlag");
+			retType = (String)retMap.get("retType");
+			if(!retFlag){
+				continue;
+			}
+			if("J".equals(retType)){
+				jbSet.add(tableName);
+			}	
+		}
+		//跑金标报送的数据
+		for(String tableName : jbSet){
+			try{
+				start=System.currentTimeMillis();
+				System.out.println("star===tableName:"+tableName);
+				
+				writeJBFile(tableName, args[0], sqlMap, tableInfoMap, jbFilePath, jbBankCode);
+				
+				end=System.currentTimeMillis();
+				System.out.println("end===time(s):["+(end-start)/1000+"]!");
+			}catch(Exception e) {
+				System.err.println(e.getMessage());
+				continue;
+			}			
+		}
+		
+	
+	} 
+	/*
+	 * 生成金标报送文件 
+	 */
+	private static void writeJBFile(String tableName, String workdate, Map sqlMap, Map tableInfoMap
+			, String filePath, String bankCode )throws Exception{
+		String fileName=null;	
+		fileName = filePath + tableName + bankCode.trim() +  ToolUtils.formatDate(workdate);
+		File datFile = new File(fileName + ".dat");
+		BufferedWriter bw = new BufferedWriter(new FileWriter(datFile));
+		
+		int count = BaseDao.queryAndWriteJBFile(tableName, workdate, sqlMap, tableInfoMap, bw);
+	
+		bw.close();
+		
+		PackZipUtil zipUtil = new PackZipUtil();
+		String zipFileName = fileName + ReportConstant.DOWN_LOAD_PACK_ZIP_EXT;
+		zipUtil.zip(zipFileName, datFile);
+		System.out.println(tableName + "file***over,sum:"+ count +"！");
+	}
 }
